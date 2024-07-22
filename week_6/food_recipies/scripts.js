f24e14664140b9ec2a0e14739e11fc6cd2ef892f
Sinan <sinancemozer@gmail.com>
2024-07-22 15:19:51 +0300

    feat:small changes

--- week_6/food_recipies/scripts.js
+++ week_6/food_recipies/scripts.js
@@ -62,9 +62,9 @@ function displayRecipes(recipes) {
     recipes.forEach(recipe => {
         const recipeItem = document.createElement('article');
         recipeItem.className = 'bg-white shadow-md rounded p-4';
-        
+
         recipeItem.innerHTML = `
-            <img src="${recipe.image}" alt="${recipe.title}" class="w-full h-48 object-cover rounded-t">
+            <img src="${recipe.image}" alt="${recipe.title}" class="w-full h-48 object-cover rounded-t mb-4">
             <h3 class="text-xl font-bold mt-4">${recipe.title}</h3>
             <p class="text-gray-700 mt-2">${recipe.category}</p>
             <a href="recipe.html?id=${recipe.id}" class="text-blue-500 hover:underline mt-4 block">Tarife Git</a>
