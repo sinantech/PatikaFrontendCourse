d743774ed6dd0d51ce747fae0010a0123099ea33
Sinan Can Özer <sinancemozer@gmail.com>
2024-07-24 20:24:29 +0300

    feat:camera section completed

---
+++ week_7/responsive_apple_website/js/main.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    var camera = new Swiper('#camera .swiper',{
+        speed:600,
+        spaceBetween:12,
+        navigation: {
+            enabled: true,
+            prevEl:'.swiper-button-prev',
+            nextEl:'.swiper-button-next',
+        }
+    })
+})(); 
